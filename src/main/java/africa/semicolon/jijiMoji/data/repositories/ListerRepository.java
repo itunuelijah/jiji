70e22f8459b49970e97f612a32c880c54927f41f
itunuelijah <itunuelijah@gmail.com>
2021-11-23 15:07:24 +0100

    jiji project

--- src/main/java/africa/semicolon/jijiMoji/data/repositories/ListerRepository.java
+++ src/main/java/africa/semicolon/jijiMoji/data/repositories/ListerRepository.java
@@ -2,7 +2,8 @@ package africa.semicolon.jijiMoji.data.repositories;
 
 import africa.semicolon.jijiMoji.data.models.Lister;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface ListerRepository extends MongoRepository<Lister, String> {
 }
--- src/main/java/africa/semicolon/jijiMoji/data/repositories/ListingRepository.java
+++ src/main/java/africa/semicolon/jijiMoji/data/repositories/ListingRepository.java
@@ -1,4 +1,9 @@
 package africa.semicolon.jijiMoji.data.repositories;
 
-public interface ListingRepository {
+import africa.semicolon.jijiMoji.data.models.Listing;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ListingRepository extends MongoRepository<Listing, String> {
 }
